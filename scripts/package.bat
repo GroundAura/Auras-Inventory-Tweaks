6850f1c7860425041f2e5da70ab009e5da9c78b3
GroundAura <52499897+GroundAura@users.noreply.github.com>
2023-09-23 09:47:55 -0600

    build name tweak

--- scripts/package.bat
+++ scripts/package.bat
@@ -4,11 +4,11 @@ cd ".."
 
 copy "docs\images\brand\Main.png" "dist\Aura's Inventory Tweaks\fomod\images"
 
-del "build\Aura's Inventory Tweaks.zip"
+del "build\Auras Inventory Tweaks.zip"
 cd "dist\Aura's Inventory Tweaks"
-"C:\Program Files\7-Zip\7z" a -tzip "..\..\build\Aura's Inventory Tweaks.zip"
+"C:\Program Files\7-Zip\7z" a -tzip "..\..\build\Auras Inventory Tweaks.zip"
 cd "..\.."
 
-copy "build\Aura's Inventory Tweaks.zip" "D:\Games\Bethesda\Elder Scrolls\Skyrim\MO2\downloads"
-copy "build\MO2\Aura's Inventory Tweaks.zip.meta" "D:\Games\Bethesda\Elder Scrolls\Skyrim\MO2\downloads"
+copy "build\Auras Inventory Tweaks.zip" "D:\Games\Bethesda\Elder Scrolls\Skyrim\MO2\downloads"
+copy "build\MO2\Auras Inventory Tweaks.zip.meta" "D:\Games\Bethesda\Elder Scrolls\Skyrim\MO2\downloads"
 
