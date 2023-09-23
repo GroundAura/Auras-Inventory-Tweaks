ebeb0898df4173d23f0746f3bbb301043af6689c
GroundAura <52499897+GroundAura@users.noreply.github.com>
2023-09-23 09:16:16 -0600

    docs and cleaning

--- scripts/md2nexus.bat
+++ scripts/md2nexus.bat
@@ -3,5 +3,5 @@ title md2nexus
 cd ".."
 
 "C:\Tools\md2nexus\md2nexus" -i "docs/description-md" -o "docs/description-nexus"
-robocopy "docs\description-md\*.txt" "docs\description-nexus"
+robocopy "docs\description-md" "docs\description-nexus" *.txt /it
 
