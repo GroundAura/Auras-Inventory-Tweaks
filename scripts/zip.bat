8bf05d6f365a9cccabc6edd2e3b9e91311211c22
GroundAura <52499897+GroundAura@users.noreply.github.com>
2024-04-23 23:12:32 -0600

    build script tweaks

--- scripts/zip.bat
+++ scripts/zip.bat
@@ -1,3 +1,11 @@
 del "build\Auras Inventory Tweaks.zip"
 cd "dist\Aura's Inventory Tweaks"
 "C:\Program Files\7-Zip\7z" a -tzip "..\..\build\Auras Inventory Tweaks.zip"
+cd ..
+del "build\AIT - Polish Translation.zip"
+cd "dist\AIT - Polish Translation"
+"C:\Program Files\7-Zip\7z" a -tzip "..\..\build\AIT - Polish Translation.zip"
+cd ..
+del "build\Auras Translation Tweaks.zip"
+cd "dist\Aura's Translation Tweaks"
+"C:\Program Files\7-Zip\7z" a -tzip "..\..\build\Auras Translation Tweaks.zip"
--- scripts/zip2.bat
+++
@@ -1,3 +0,0 @@
-del "build\AIT - Polish Translation.zip"
-cd "dist\AIT - Polish Translation"
-"C:\Program Files\7-Zip\7z" a -tzip "..\..\build\AIT - Polish Translation.zip"
